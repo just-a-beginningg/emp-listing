28337af3ed5c0f512ccf4aed89c1e2f8b527b8e3
Arun <youngdeveloper37@gmail.com>
2024-06-28 20:58:11 +0530

    FIX :: Add Footer component

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ListingService } from 'src/app/pages/listing/listing.service';
+import { Employee } from '../types';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private listingService: ListingService) {}
+  page = 0;
+  totalPages = 0;
+  totalLength = 0;
+  list: Employee[] = [];
   ngOnInit(): void {
+    this.page = Number(localStorage.getItem('page')) ?? 0;
+    this.totalPages = Number(localStorage.getItem('total')) ?? 0;
+    this.totalLength = Number(localStorage.getItem('totalLength')) ?? 0;
+    this.listingService.list$.subscribe({
+      next: (res) => {
+        if (res) {
+          this.list = res;
+        }
+      },
+    });
   }
 
+  onClickPrev() {
+    if (this.page == 1) {
+      return;
+    }
+    this.listingService.prevList();
+    this.page = Number(localStorage.getItem('page')) ?? 0;
+    this.totalPages = Number(localStorage.getItem('total')) ?? 0;
+  }
+  onClickNext() {
+    if (this.page == this.totalPages) {
+      return;
+    }
+    this.listingService.nextList();
+    this.page = Number(localStorage.getItem('page')) ?? 0;
+    this.totalPages = Number(localStorage.getItem('total')) ?? 0;
+  }
 }

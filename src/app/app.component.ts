a80fd91558120d90d742ee0a4aeb5e45f0210a3c
Arun <youngdeveloper37@gmail.com>
2024-06-28 21:00:28 +0530

    FIX :: Refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'employee';
+  showNavbar = false;
+
+  constructor(private route:Router){}
+  ngOnInit(): void {
+      console.log(this.route.url)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
+import { SharedModule } from './shared/navbar/shared.module';
 
 @NgModule({
   declarations: [AppComponent, ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule,SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
